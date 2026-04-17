9b6dc72ed745fd220f7645cd279e1213ff615f3a
awoladhossain <awoladh04@gmail.com>
2026-04-17 09:52:07 +0600

    auth module completed

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,10 @@
 import { CurrentUser } from '@/common/decorators/current-user.decorator';
+import { JwtAuthGuard } from '@/common/guards/jwt-auth.guard';
 import { JwtRefreshGuard } from '@/common/guards/jwt-refresh.guard';
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
   Post,
@@ -10,6 +12,7 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
 import { RegisterDto } from './dto/register.dto';
 
 interface RequestUser {
@@ -38,10 +41,37 @@ export class AuthController {
     return this.authService.login(dto);
   }
 
+  /**
+   * user -> /auth/refrersh -> useguard ->
+   */
   @Post('refresh')
   @HttpCode(HttpStatus.OK)
   @UseGuards(JwtRefreshGuard)
   refresh(@CurrentUser() user: RequestUser) {
     return this.authService.refreshTokens(user.id, user.refreshToken);
+  }
+
+  // * logout
+  @Post('logout')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(JwtAuthGuard)
+  logout(@CurrentUser('id') userId: string, @Body() dto: RefreshTokenDto) {
+    return this.authService.logout(userId, dto.refreshToken);
+  }
+
+  // * logout all
+  @Post('logout-all')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(JwtAuthGuard)
+  logoutAll(@CurrentUser('id') userId: string) {
+    return this.authService.logoutAll(userId);
+  }
+
+  // * get profile
+  @Get('me')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(JwtAuthGuard)
+  getProfile(@CurrentUser('id') userId: string) {
+    return this.authService.getProfile(userId);
   }
 }
