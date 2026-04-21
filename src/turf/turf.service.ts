48706af4f886c9ce95eccbbfa7b492889e8f4e3c
awoladhossain <awoladh04@gmail.com>
2026-04-21 22:10:13 +0600

    feat: turf service

--- src/turf/turf.service.ts
+++ src/turf/turf.service.ts
@@ -2,6 +2,7 @@ import { PrismaService } from '@/prisma/prisma.service';
 import { RedisLockService } from '@/redis/redis-lock.service';
 import { Injectable } from '@nestjs/common';
 import { CreateTurfDto } from './dto/create-turf.dto';
+import { QueryTurfDto } from './dto/query-turf.dto';
 
 @Injectable()
 export class TurfService {
@@ -14,7 +15,15 @@ export class TurfService {
     const turf = await this.prisma.turf.create({
       data: dto,
     });
+    // Invalidate cache for turf list after creating a new turf to ensure fresh data on next fetch
     await this.redis.delByPattern('turfs:list:*');
     return turf;
+  }
+
+  async findAll(query: QueryTurfDto){
+    const {city, sportType, search, page=1, limit=10} = query;
+
+    // cache key based on query params
+    const cacheKey = `turfs:list:${JSON.stringify(query)}`;
   }
 }
