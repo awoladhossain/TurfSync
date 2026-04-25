172edae8aad751f75fa8005471315177b459043e
awoladhossain <awoladh04@gmail.com>
2026-04-25 14:15:19 +0600

    feat: turf service on going

--- src/turf/turf.service.ts
+++ src/turf/turf.service.ts
@@ -1,7 +1,9 @@
 import { PrismaService } from '@/prisma/prisma.service';
 import { RedisLockService } from '@/redis/redis-lock.service';
 import { ConflictException, Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { CreateTurfDto } from './dto/create-turf.dto';
+import { QueryTurfDto } from './dto/query-turf.dto';
 
 @Injectable()
 export class TurfService {
@@ -27,5 +29,56 @@ export class TurfService {
     });
     await this.redis.delByPattern(`turf:list:*`);
     return turf;
+  }
+
+  // find all
+  async findAll(query: QueryTurfDto) {
+    const { city, sportType, search, page = 1, limit = 10 } = query;
+    // cache key
+    const cacheKey = `turf:list:${JSON.stringify(query)}`;
+
+    const cached = await this.redis.get(cacheKey);
+    if (cached) {
+      return cached;
+    }
+    const skip = (page - 1) * limit;
+    const where: Prisma.TurfWhereInput = { isActive: true };
+
+    if (city) {
+      where.city = { contains: city, mode: 'insensitive' };
+    }
+    if (sportType) {
+      where.sportType = sportType;
+    }
+    if (search) {
+      where.name = { contains: search, mode: 'insensitive' };
+    }
+
+    const [turfs, total] = await Promise.all([
+      this.prisma.turf.findMany({
+        where,
+        skip,
+        take: limit,
+        orderBy: { createdAt: 'desc' },
+      }),
+      this.prisma.turf.count({ where }),
+    ]);
+
+    const result = {
+      data: turfs,
+      meta: {
+        total,
+        page,
+        limit,
+        totalPages: Math.ceil(total / limit),
+      },
+    };
+    await this.redis.set(cacheKey, result, 300); // cache for 5 minutes
+    return result;
+  }
+
+  // find one by id
+  async findOne(id: string) {
+    const cacheKey = `turf:${id}`;
   }
 }
