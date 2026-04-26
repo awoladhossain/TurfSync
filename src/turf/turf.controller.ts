6f5782072a22d943f8e33f1b7a6ba714f53e8c0a
awoladhossain <awoladh04@gmail.com>
2026-04-26 20:36:26 +0600

    feat: turf controller initial

--- src/turf/turf.controller.ts
+++ src/turf/turf.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Roles } from '@/common/decorators/roles.decorator';
+import { JwtAuthGuard } from '@/common/guards/jwt-auth.guard';
+import { RolesGuard } from '@/common/guards/roles.guard';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { Role } from '@prisma/client';
+import { CreateTurfDto } from './dto/create-turf.dto';
+import { QueryTurfDto } from './dto/query-turf.dto';
+import { TurfService } from './turf.service';
 
 @Controller('turf')
-export class TurfController {}
+export class TurfController {
+  constructor(private turfService: TurfService) {}
+
+  @Get()
+  findAll(@Query() query: QueryTurfDto) {
+    return this.turfService.findAll(query);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.turfService.findOne(id);
+  }
+
+  @Get(':id/slots')
+  getSlots(@Param('id') id: string, @Query('date') date: string) {
+    return this.turfService.getAvailableSlots(id, date);
+  }
+
+  @Post()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  create(@Body() dto: CreateTurfDto) {
+    return this.turfService.create(dto);
+  }
+}
