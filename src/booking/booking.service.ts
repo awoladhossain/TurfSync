95d2d4a7875845b9b1aa1dade7812488b9ce7915
awoladhossain <awoladh04@gmail.com>
2026-04-27 21:14:38 +0600

    feat: booking system initial

--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -2,7 +2,13 @@ import { PrismaService } from '@/prisma/prisma.service';
 import { NOTIFICATION_QUEUE } from '@/queue/queue.constant';
 import { RedisLockService } from '@/redis/redis-lock.service';
 import { InjectQueue } from '@nestjs/bull';
-import { Injectable, Logger } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import type { Queue } from 'bull';
 import { CreateBookingDto } from './dto/create-booking.dto';
 
@@ -18,5 +24,33 @@ export class BookingService {
 
   async create(dto: CreateBookingDto, userId: string) {
     const lockKey = `slot:${dto.slotId}`;
+
+    const lockAcquired = await this.redis.acquireLock(lockKey, 30);
+    if (!lockAcquired) {
+      throw new ConflictException(
+        `Slot ${dto.slotId} is currently being booked by another user. Please try again later.`,
+      );
+    }
+    try {
+      const slot = await this.prisma.slot.findUnique({
+        where: {
+          id: dto.slotId,
+        },
+        include: { turf: true },
+      });
+      if (!slot) {
+        throw new NotFoundException(`Slot with id ${dto.slotId} not found`);
+      }
+      if (slot.turfId !== dto.turfId) {
+        throw new BadRequestException(
+          'Slot does not belong to the specified turf',
+        );
+      }
+
+      if (slot.isBooked) {
+        throw new ConflictException('Slot is already booked');
+      }
+    } finally {
+    }
   }
 }
