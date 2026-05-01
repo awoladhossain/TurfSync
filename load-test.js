7cfc34862377d5d55b8428d5a37ab3a5bbec9ecb
awoladhossain <awoladh04@gmail.com>
2026-05-01 19:14:38 +0600

    feat: summary report

--- load-test.js
+++ load-test.js
@@ -1,11 +1,12 @@
 import { check, sleep } from 'k6';
 import http from 'k6/http';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
   stages: [
-    { duration: '30s', target: 20 }, // ৩০ সেকেন্ডে ইউজার ০ থেকে ২০ এ উঠবে
-    { duration: '1m', target: 20 }, // পরবর্তী ১ মিনিট ২০ জন ইউজার টানা রিকোয়েস্ট পাঠাবে
-    { duration: '20s', target: 0 }, // শেষ ২০ সেকেন্ডে ইউজার ০ তে নেমে আসবে
+    { duration: '30s', target: 20 }, 
+    { duration: '1m', target: 20 },
+    { duration: '20s', target: 0 }, 
   ],
 };
 
@@ -16,5 +17,11 @@ export default function () {
     'status is 200': (r) => r.status === 200,
     'transaction time < 200ms': (r) => r.timings.duration < 200,
   });
-  sleep(1); // প্রতিটি রিকোয়েস্টের মাঝে ১ সেকেন্ডের বিরতি
+  sleep(1); 
+}
+
+export function handleSummary(data) {
+  return {
+    "summary.html": htmlReport(data),
+  };
 }
