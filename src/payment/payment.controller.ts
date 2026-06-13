13bdc9bff4ff849eac17144f6799b2fce2fecff5
awoladhossain <awoladh04@gmail.com>
2026-06-13 11:52:19 +0600

    feat(payment): ✨ add create-payment-intent endpoint

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { CurrentUser } from '@/common/decorators/current-user.decorator';
+import { JwtAuthGuard } from '@/common/guards/jwt-auth.guard';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { CreatePaymentDto } from './dto/create-payment.dto';
+import { PaymentService } from './payment.service';
 
 @Controller('payment')
-export class PaymentController {}
+export class PaymentController {
+  constructor(private paymentService: PaymentService) {}
+
+  // payment intent
+  @Post('create-payment-intent')
+  @UseGuards(JwtAuthGuard)
+  createPaymentIntent(
+    @Body() dto: CreatePaymentDto,
+    @CurrentUser('id') userId: string,
+  ) {
+    return this.paymentService.createPaymentIntent(dto, userId);
+  }
+}
