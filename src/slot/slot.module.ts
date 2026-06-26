e72133a3138bd4f3a0e04055c5f3943038ec895b
awoladhossain <awoladh04@gmail.com>
2026-06-27 00:37:21 +0600

    refactor: remove redundant ScheduleModule.forRoot() from SlotModule

--- src/slot/slot.module.ts
+++ src/slot/slot.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ScheduleModule } from '@nestjs/schedule';
 import { SlotController } from './slot.controller';
 import { SlotService } from './slot.service';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
   providers: [SlotService],
   controllers: [SlotController],
   exports: [SlotService],
