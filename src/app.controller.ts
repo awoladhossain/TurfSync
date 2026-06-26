4987854f61302b778c67fc3aedb3f5c2013305c8
awoladhossain <awoladh04@gmail.com>
2026-06-27 00:43:10 +0600

    feat: inject SlotService in AppService to generate slots on bootstrap

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,11 +4,6 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
   @Get('/debug-sentry')
   getError() {
     throw new Error('My first Sentry error!');
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common';
+import { SlotService } from './slot/slot.service';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements OnApplicationBootstrap {
+  private readonly logger = new Logger(AppService.name);
+  constructor(private slotService: SlotService) {}
+
+  // app will start
+  async onApplicationBootstrap() {
+    this.logger.log('🏟️ Generating initial slots...');
+    await this.slotService.generateSlotsForNextDays(30);
+    this.logger.log('✅ Initial slot generation done');
   }
 }
