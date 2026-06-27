315c156919d200e722e7ea96b7674e7fb33b51fb
awoladhossain <awoladh04@gmail.com>
2026-06-27 16:37:59 +0600

    chore(upload): install @nestjs/platform-express and inject dependencies into UploadController

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,4 +1,17 @@
+import { PrismaService } from '@/prisma/prisma.service';
 import { Controller } from '@nestjs/common';
+import { UploadService } from './upload.service';
 
 @Controller('upload')
-export class UploadController {}
+export class UploadController {
+  constructor(
+    private uploadService: UploadService,
+    private prisma: PrismaService,
+  ) {}
+
+  // @Post('turf/:turfId/image')
+  // @UseInterceptors(FileInterceptor('image', {
+  //   storage: memoryStorage(),
+  //   limits: { fileSize: 5 * 1024 * 1024 }
+  // }))
+}
