349325fb1d85804efb760e9cd7c581da8cdc1ae7
awoladhossain <awoladh04@gmail.com>
2026-07-15 22:41:54 +0600

    feat(sms): remove unused controller and export SmsService in SmsModule

--- src/sms/sms.module.ts
+++ src/sms/sms.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SmsService } from './sms.service';
-import { SmsController } from './sms.controller';
 
 @Module({
   providers: [SmsService],
-  controllers: [SmsController],
+  exports: [SmsService],
 })
 export class SmsModule {}
